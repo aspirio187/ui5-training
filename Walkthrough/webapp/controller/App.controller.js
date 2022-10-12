e25212271d202368078bb56be547b5560c471ec7
Soultan <soultan_98@hotmail.com>
2022-10-12 15:31:22 +0200

    Step 7: JSON Model

--- Walkthrough/webapp/controller/App.controller.js
+++ Walkthrough/webapp/controller/App.controller.js
@@ -1,11 +1,22 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], function (Controller, MessageToast) {
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
     "user strict";
 
-    return Controller.extend(
-        "sap.ui.demo.Walkthrough.controller.App", {
+    return Controller.extend("sap.ui.demo.Walkthrough.controller.App", {
+
+        onInit: function () {
+            var oData = {
+                recipient: {
+                    name: "World"
+                }
+            };
+            var oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);
+        },
+
         onShowHello: function () {
             MessageToast.show("Hello World!");
         }
